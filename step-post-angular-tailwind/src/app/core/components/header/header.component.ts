f8a0b58cb00186dd0413a2c311e567f2839ae692
CyrilPonsan <cyril.ponsan@gmail.com>
2022-09-29 07:22:40 +0200

    adresse form

--- step-post-angular-tailwind/src/app/core/components/header/header.component.ts
+++ step-post-angular-tailwind/src/app/core/components/header/header.component.ts
@@ -6,7 +6,6 @@ import { AuthService } from '../../services/auth.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
   constructor(public auth: AuthService, private router: Router) {}
