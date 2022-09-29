967658d5cecfd6f056b5141491148b754d8bf813
CyrilPonsan <cyril.ponsan@gmail.com>
2022-09-29 12:37:21 +0200

    icones

--- step-post-angular/src/environments/environment.ts
+++ step-post-angular/src/environments/environment.ts
@@ -6,13 +6,17 @@
 export const environment = {
   production: true,
   // baseUrl: 'http://localhost:3000/api',
-  baseUrl: 'https://step-post-nodejs.herokuapp.com/api',
-  mailRegex:
-    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-  numberRegex: /^[0-9]*$/,
-  passwordRegex:
-    /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/,
-  genericRegex: /^[a-zA-Z0-9\s,.'-+éàèù]{0,}$/,
+  url: {
+    baseUrl: 'https://step-post-nodejs.herokuapp.com/api',
+  },
+  regex: {
+    mailRegex:
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    numberRegex: /^[0-9]*$/,
+    passwordRegex:
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/,
+    genericRegex: /^[a-zA-Z0-9\s,.'-+éàèù]{0,}$/,
+  },
 };
 
 /*
