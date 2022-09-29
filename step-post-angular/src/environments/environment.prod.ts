660d32a63f0d4ed9131af4a960d8ed7c81e0870b
CyrilPonsan <cyril.ponsan@gmail.com>
2022-09-29 10:34:30 +0200

    env

--- step-post-angular/src/environments/environment.prod.ts
+++ step-post-angular/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  // baseUrl: 'http://localhost:3000/api',
+  baseUrl: 'https://step-post-nodejs.herokuapp.com/api',
+  mailRegex:
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+  numberRegex: /^[0-9]*$/,
+  passwordRegex:
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/,
+  genericRegex: /^[a-zA-Z0-9\s,.'-+éàèù]{0,}$/,
 };
