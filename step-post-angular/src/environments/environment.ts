a97a630927ce32ecbdcb0848d0a5254ffc2ec31f
CyrilPonsan <cyril.ponsan@gmail.com>
2022-11-16 16:32:41 +0100

    aws

--- step-post-angular/src/environments/environment.ts
+++ step-post-angular/src/environments/environment.ts
@@ -6,7 +6,7 @@
 export const environment = {
   production: true,
   url: {
-    baseUrl: 'https://step-post-nodejs.herokuapp.com/api',
+    baseUrl: 'http://dev01.step.eco:3000/api',
     //baseUrl: 'http://localhost:3000/api',
   },
   regex: {
