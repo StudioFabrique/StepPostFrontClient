09aee69c80d7717049e35f4e443351218ab8fd00
CyrilPonsan <cyril.ponsan@gmail.com>
2022-12-15 12:48:27 +0100

    footer

--- step-post-angular/src/app/core/components/footer/footer.component.ts
+++ step-post-angular/src/app/core/components/footer/footer.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
+  contactEmail = 'blom@step.eco';
+  contactPhone = '05 64 27 01 05';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
